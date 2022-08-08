1ded99de9e8143fd343351b56eaa421d552bd860
kuldeeptomar07 <kuldeeptomar07@gmail.com>
2022-08-08 19:26:33 +0530

    register3

---
+++ Controllers/RegisterController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class RegisterController
+        Inherits Controller
+
+        ' GET: Register
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
